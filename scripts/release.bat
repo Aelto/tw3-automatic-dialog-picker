80dd5898e1ea16192cdfc2b53125e2d22c07166f
Adam Sunderman <amsunderman@gmail.com>
2023-01-06 22:53:12 -0600

    modified release.bat to have release structure work better for a single mod package

--- scripts/release.bat
+++ scripts/release.bat
@@ -4,10 +4,10 @@ call encode-csv-strings.bat
 rmdir "%modpath%\release" /s /q
 mkdir "%modpath%\release"
 
-mkdir "%modpath%\release\mods\%modname%\content\scripts\"
-rmdir "%modpath%\release\mods\%modName%\content\" /s /q
-XCOPY "%modpath%\src" "%modpath%\release\mods\%modName%\content\scripts\" /e /s /y
-XCOPY "%modpath%\strings" "%modpath%\release\mods\%modName%\content\" /e /s /y
+mkdir "%modpath%\release\%modname%\content\scripts\"
+rmdir "%modpath%\release\%modName%\content\" /s /q
+XCOPY "%modpath%\src" "%modpath%\release\%modName%\content\scripts\" /e /s /y
+XCOPY "%modpath%\strings" "%modpath%\release\%modName%\content\" /e /s /y
 
-mkdir "%modpath%\release\bin\config\r4game\user_config_matrix\pc\"
-copy "%modpath%\mod-menu.xml" "%modpath%\release\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
+mkdir "%modpath%\release\%modName%\bin\config\r4game\user_config_matrix\pc\"
+copy "%modpath%\mod-menu.xml" "%modpath%\release\%modName%\bin\config\r4game\user_config_matrix\pc\%modname%.xml" /y
